21505e1b89759d779826fb1b9bc32233943c05cb
fonald <dkcook90@gmail.com>
2022-02-18 20:26:15 -0700

    Added timer functionality and score keeping

--- assets/script.js
+++ assets/script.js
@@ -1,83 +1,113 @@
 var startBtn = document.querySelector('#start-btn')
 var question = document.querySelector('#question')
-var answers = document.querySelector('#answers')
+// var answerOne = document.querySelector('#answerOne')
+// var answerTwo = document.querySelector('#answerTwo')
+// var answerThree = document.querySelector('#answerThree')
+// var answerFour = document.querySelector('#answerFour')
 var currentScore = document.querySelector('#currentScore')
+var answers = document.querySelector('#answers')
+var answerList = document.querySelector('#gamebox')
 var score = 0
+var acceptableAnwers = ["Yoshi", "Waffles", "Pit Bull", "It didn't have to be"]
+var questions = ["What is the best Super Mario Character?", "What is the best breakfast food?", "Who is Mr. Worldwide?", "Why was this so hard?"]
+var firstAnswer = ["Mario", "Sausage", "Snoop Doggy Dog", "That's js baybay"]
+var secondAnswer = ["Luigi", "Pancakes", "Tech Nine", "It didnt have to be"]
+var thirdAnswer = ["Yoshi", "Cereal", "Kane", "It really wasn't THAT hard"]
+var fourthAnswer = ["Toad", "Waffles", "Pit Bull", "Dude....."]
+var questionIndex = 0
+var answerKey = 0
+var timeLeft = 30
 
-function quiz() {
-    var availableQs = ['What is the best flavor of ice-cream?', 'Who is the best actor?', 'What are you wearing?', 'Why would I ask that?']
-    var answersOne = ['Chocolate', 'Vanilla', 'Mint', 'Strawberry']
-    var answersTwo = ['Dwane Johnson', 'John Cena', 'Brock Lesner', 'Kane']
-
-    question.textContent=availableQs[0]
-    
-    for (i = 0; i < answersOne.length; i++) {
-    
-        var selectableAnswer = document.createElement('li')
-        var selectableAnswerBtn = document.createElement('button')
-    
-        answers.appendChild(selectableAnswer)
-    
-        selectableAnswer.appendChild(selectableAnswerBtn)
-    
-        selectableAnswerBtn.textContent=answersOne[i]
-    
-        selectableAnswerBtn.addEventListener('click', function(event) {
-            event.preventDefault();
-    
-            // console.log(event)
-    
-            if (event.target.textContent === "Chocolate") {
-                window.alert('Nice!')
-                score++
-                currentScore.textContent=score
-    
-            }
-            else {
-                window.alert('Not right...')
-            }
-        })
-    
+function checkTime() {
+    if (timeLeft <= 0) {
+        timerDisplay.style.display='none'
+        answerList.removeChild('ul')
+        question.textContent="TIMES UP!"
     }
+}
 
-    question.textContent=availableQs[1]
+
+
+function timerCount() {
+
+    var timerDisplay = document.querySelector('#timer')
+    timerDisplay.textContent=timeLeft
+    timeLeft--
+    checkTime()
+}
+
+
+
+function FirstQuestion() {
+
+    question.textContent=questions[questionIndex]
+    // answers.style.display='block'
+    // answerOne.textContent=firstAnswer[questionIndex]
+    // answerTwo.textContent=secondAnswer[questionIndex]
+    // answerThree.textContent=thirdAnswer[questionIndex]
+    // answerFour.textContent=fourthAnswer[questionIndex]
+    var answerOne = document.createElement('li')
+    var answerTwo = document.createElement('li')
+    var answerThree = document.createElement('li')
+    var answerFour = document.createElement('li')
+    var answerOneBtn = document.createElement('button')
+    var answerTwoBtn = document.createElement('button')
+    var answerThreeBtn = document.createElement('button')
+    var answerFourBtn = document.createElement('button')
+
+    answerOneBtn.setAttribute('class', 'answerBtn')
+    answerTwoBtn.setAttribute('class', 'answerBtn')
+    answerThreeBtn.setAttribute('class', 'answerBtn')
+    answerFourBtn.setAttribute('class', 'answerBtn')
+
+    answers.appendChild(answerOne)
+    answerOne.appendChild(answerOneBtn)
+    answerOneBtn.textContent=firstAnswer[questionIndex]
+    answers.appendChild(answerTwo)
+    answerTwo.appendChild(answerTwoBtn)
+    answerTwoBtn.textContent=secondAnswer[questionIndex]
+    answers.appendChild(answerThree)
+    answerThree.appendChild(answerThreeBtn)
+    answerThreeBtn.textContent=thirdAnswer[questionIndex]
+    answers.appendChild(answerFour)
+    answerFour.appendChild(answerFourBtn)
+    answerFourBtn.textContent=fourthAnswer[questionIndex]
+
+    var answerBtn = document.querySelectorAll('.answerBtn')
+
     
-    for (i = 0; i < answersTwo.length; i++) {
-    
-        var selectableAnswer = document.createElement('li')
-        var selectableAnswerBtn = document.createElement('button')
-    
-        answers.appendChild(selectableAnswer)
-    
-        selectableAnswer.appendChild(selectableAnswerBtn)
-    
-        selectableAnswerBtn.textContent=answersTwo[i]
-    
-        selectableAnswerBtn.addEventListener('click', function(event) {
-            event.preventDefault();
-    
-            // console.log(event)
-    
-            if (event.target.textContent === "Kane") {
-                window.alert('Nice!')
-                score++
-                currentScore.textContent=score
-    
-            }
-            else {
-                window.alert('Not right...')
-            }
-        })
-    
+
+    for (i = 0; i < answerBtn.length; i++){
+
+        answerBtn[i].addEventListener('click', function(event) {
+                event.preventDefault();
+        
+                if (this.textContent === acceptableAnwers[answerKey]) {
+                    window.alert('Nice!')
+                    score++
+                    answerKey++
+                    questionIndex++
+                    currentScore.textContent=score
+                    answers.innerHTML=""
+                    FirstQuestion()
+                }
+                else {
+                    window.alert('Not right...')
+                    answerKey++
+                    questionIndex++
+                    answers.innerHTML=""
+                    FirstQuestion()
+                }
+            })
     }
-}    
+}
+   
 
     
 startBtn.addEventListener('click', function(event) {
     event.preventDefault();
-
     question.textContent=''
     startBtn.style.display='none'
-    
-    quiz()
+    var timer = setInterval(timerCount, 1000)
+    FirstQuestion()
 })
